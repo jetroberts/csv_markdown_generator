a6275e1e348092e774d00e57f29fac19b511fc2d
jetroberts <j-et@live.co.uk>
2023-03-20 11:42:55 +0000

    fix: handle empty filepath

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
-use std::{fs::File, io::BufReader, env};
+use std::{env, fs::File, io::BufReader};
 
 mod markdown_table;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        println!("No file provided");
+        return;
+    }
+
     let filepath = &args[1];
+    if *filepath == String::new() {
+        return;
+    }
+
     let file = File::open(filepath).unwrap();
     let reader = BufReader::new(file);
 
